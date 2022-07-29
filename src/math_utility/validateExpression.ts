f3c9a3f6d13bcd04e559787d58e8e903502bbcdb
Aleksei Treshchalin <treshchal@gmail.com>
2022-07-29 22:01:47 +0300

    Added token type enum

--- src/math_utility/validateExpression.ts
+++ src/math_utility/validateExpression.ts
@@ -1,16 +1,24 @@
+import { expressionTokenType } from "../utility/enums";
+
 function validateExpression(expression: string): boolean {
   const arrayedExpression = expression.split("");
   let openingParenthesis = 0;
   console.log(arrayedExpression);
-  let prevToken = {
-    tokenType: "beginning",
+
+  interface token {
+    tokenType: expressionTokenType;
+    decimalPointUsed: Boolean;
+  }
+
+  let prevToken: token = {
+    tokenType: expressionTokenType.BEGINNING,
     decimalPointUsed: false,
   };
   try {
     arrayedExpression.forEach((token) => {
       switch (token) {
         case "-":
-          prevToken.tokenType = "operator";
+          prevToken.tokenType = expressionTokenType.OPERATOR;
           prevToken.decimalPointUsed = false;
           break;
         case "+":
@@ -18,43 +26,43 @@ function validateExpression(expression: string): boolean {
         case "/":
         case "^":
           if (
-            prevToken.tokenType !== "number" &&
-            prevToken.tokenType !== "rightParenthesis"
+            prevToken.tokenType !== expressionTokenType.NUMBER &&
+            prevToken.tokenType !== expressionTokenType.RIGHT_PARENTHESIS
           ) {
             throw "Binary operator must be after a number or a right parenthesis";
           }
-          prevToken.tokenType = "operator";
+          prevToken.tokenType = expressionTokenType.OPERATOR;
           prevToken.decimalPointUsed = false;
           break;
         case "(":
           if (
-            prevToken.tokenType === "number" ||
-            prevToken.tokenType === "decimalPoint" ||
-            prevToken.tokenType === "rightParenthesis"
+            prevToken.tokenType === expressionTokenType.NUMBER ||
+            prevToken.tokenType === expressionTokenType.DECIMAL_POINT ||
+            prevToken.tokenType === expressionTokenType.RIGHT_PARENTHESIS
           ) {
             throw "Left parenthesis can't be after by number, decimal point of right parenthesis";
           }
           openingParenthesis++;
-          prevToken.tokenType = "leftParenthesis";
+          prevToken.tokenType = expressionTokenType.LEFT_PARENTHESIS;
           prevToken.decimalPointUsed = false;
           break;
         case ")":
           if (openingParenthesis <= 0) {
             throw "Detected non matching parenthesis";
-          } else if (prevToken.tokenType !== "number") {
+          } else if (prevToken.tokenType !== expressionTokenType.NUMBER) {
             throw "Right parenthesis must be after a number";
           }
           openingParenthesis--;
-          prevToken.tokenType = "rightParenthesis";
+          prevToken.tokenType = expressionTokenType.RIGHT_PARENTHESIS;
           prevToken.decimalPointUsed = false;
           break;
         case ".":
           if (prevToken.decimalPointUsed === true) {
             throw "Detected multiple decimal points in a number";
-          } else if (prevToken.tokenType !== "number") {
+          } else if (prevToken.tokenType !== expressionTokenType.NUMBER) {
             throw "Decimal point must be between numbers";
           }
-          prevToken.tokenType = "decimalPoint";
+          prevToken.tokenType = expressionTokenType.DECIMAL_POINT;
           prevToken.decimalPointUsed = true;
           break;
         case "0":
@@ -67,10 +75,10 @@ function validateExpression(expression: string): boolean {
         case "7":
         case "8":
         case "9":
-          if (prevToken.tokenType === "rightParenthesis") {
+          if (prevToken.tokenType === expressionTokenType.RIGHT_PARENTHESIS) {
             throw "Number can't be after right parenthesis";
           }
-          prevToken.tokenType = "number";
+          prevToken.tokenType = expressionTokenType.NUMBER;
           break;
       }
     });
@@ -78,8 +86,8 @@ function validateExpression(expression: string): boolean {
       throw "Detected non matching parenthesis";
     }
     if (
-      prevToken.tokenType !== "rightParenthesis" &&
-      prevToken.tokenType !== "number"
+      prevToken.tokenType !== expressionTokenType.RIGHT_PARENTHESIS &&
+      prevToken.tokenType !== expressionTokenType.NUMBER
     ) {
       throw "expression must end with a right parenthesis or a number";
     }
