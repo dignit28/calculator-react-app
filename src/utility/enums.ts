505f1ee3bd8d37a156e3975cb40872d4fe13b3e9
Aleksei Treshchalin <treshchal@gmail.com>
2022-07-29 22:03:36 +0300

    Empty enum file fix

---
+++ src/utility/enums.ts
@@ -0,0 +1,8 @@
+export enum expressionTokenType {
+  BEGINNING = "beginning",
+  OPERATOR = "operator",
+  NUMBER = "number",
+  LEFT_PARENTHESIS = "leftParenthesis",
+  RIGHT_PARENTHESIS = "rightParenthesis",
+  DECIMAL_POINT = "decimalPoint",
+}
